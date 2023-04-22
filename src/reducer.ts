6b369b3d061c55cd89a18a8977cea577a6cf2ee5
Nathan <nslee333@gmail.com>
2023-04-22 10:47:39 -0700

    Added explicit types for reducer function

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,11 +1,19 @@
-export function reducer(state: any, action: any) {
-  if (action.type === 'incremented_count') {
+type Action = {
+  type: string;
+};
+
+type State = {
+  count: number;
+};
+
+export function reducer(state: State, action: Action) {
+  if (action.type === "incremented_count") {
     return {
-      count: state.count + 1
+      count: state.count + 1,
     };
-  } else if (action.type === 'decremented_count') {
+  } else if (action.type === "decremented_count") {
     return {
-      count: state.count - 1
+      count: state.count - 1,
     };
   } else {
     throw Error("Unknown Error.");
